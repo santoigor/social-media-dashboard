9cc3f428f0d926fdb34692083970b44b464277f4
Igor Alves <igor.asantosss@outlook.com>
2021-07-25 15:49:05 -0300

    array de conteúdo para serem consumidos na seção overview criados

--- src/Data.js
+++ src/Data.js
@@ -44,4 +44,90 @@ export const info = {
             }
         },
     ] 
+};
+
+export const overviewInfo = {
+    data: [
+        {
+            id: '1',
+            title: "Page views",
+            social_media: 'facebook',
+            number: '87',
+            percentage: {
+                percent: '3%',
+                diference: 'up'
+            }
+        },
+        {
+            id: '2',
+            title: "Likes",
+            social_media: 'facebook',
+            number: '52',
+            percentage: {
+                percent: '2%',
+                diference: 'down'
+            }
+        },
+        {
+            id: '3',
+            title: "Likes",
+            social_media: 'instagram',
+            number: '5462',
+            percentage: {
+                percent: '2257%',
+                diference: 'up'
+            }
+        },
+        {
+            id: '4',
+            title: "Profile Views",
+            social_media: 'instagram',
+            number: '52k',
+            percentage: {
+                percent: '1375%',
+                diference: 'up'
+            }
+        },
+        {
+            id: '5',
+            title: "Retweets",
+            social_media: 'twitter',
+            number: '117',
+            percentage: {
+                percent: '303%',
+                diference: 'up'
+            }
+        },
+        {
+            id: '6',
+            title: "Likes",
+            social_media: 'twitter',
+            number: '507',
+            percentage: {
+                percent: '553%',
+                diference: 'up'
+            }
+        },
+
+        {
+            id: '7',
+            title: "Likes",
+            social_media: 'youtube',
+            number: '107',
+            percentage: {
+                percent: '19%',
+                diference: 'down'
+            }
+        },
+        {
+            id: '8',
+            title: "Total Views",
+            social_media: 'youtube',
+            number: '1407',
+            percentage: {
+                percent: '12%',
+                diference: 'down'
+            }
+        },
+    ]
 }
